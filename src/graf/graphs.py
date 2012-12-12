07b52fe455f4be1acfbd6cc82e19789236be640b
LopesAntonioM <lopes.antoniomr@gmail.com>
2012-12-12 15:34:57 +0000

    Error parsing header file fixed.

--- src/graf/graphs.py
+++ src/graf/graphs.py
@@ -130,7 +130,10 @@ class Graph(object):
                 id = 'e%d' % self._top_edge_id
                 self._top_edge_id += 1
         res = Edge(id, from_node, to_node)
-        self.edges.add(res)
+
+        if not res in self.edges.values():
+            self.edges.add(res)
+
         return res
 
     def find_edge(self, from_node, to_node):
